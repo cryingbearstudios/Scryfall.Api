f9e0483c895c9bf34e37af01f28890de13ae150d
Matt Campbell <xenolinguist@gmail.com>
2019-09-18 20:46:51 -0500

    Add rate-limiting to ApiClient

--- src/Scryfall.Api/ApiClient.cs
+++ src/Scryfall.Api/ApiClient.cs
@@ -7,18 +7,11 @@ using Newtonsoft.Json;
 
 namespace Scryfall.Api
 {
-    public class ScryfallApiError : Exception
-    {
-        public Error Error { get; }
-
-        public ScryfallApiError(Error error)
-        {
-            Error = error;
-        }
-    }
-
     public partial class ApiClient
     {
+        private const int RateLimitDelayMilliseconds = 50;
+        private Task _rateLimitingTask = Task.FromResult(true);
+
         private readonly HttpClient _client;
         private readonly JsonSerializer _serializer;
 
@@ -28,31 +21,27 @@ namespace Scryfall.Api
             _serializer = serializer;
         }
 
-        public void Dispose()
-        {
-            _client.Dispose();
-        }
+        public void Dispose() => _client.Dispose();
 
         private async Task<T> SendRequest<T>(HttpMethod method, string path, NameValueCollection query)
         {
+            await _rateLimitingTask;
             var requestUri = new UriBuilder(_baseUri) {Path = path, Query = query.ToString()}.Uri;
-            using (var request = new HttpRequestMessage {Method = method, RequestUri = requestUri})
-            using (var response = await _client.SendAsync(request))
-            {
-                if (!response.IsSuccessStatusCode)
-                {
-                    throw new ScryfallApiError(await ReadContentAsJson<Error>(response));
-                }
+            var request = new HttpRequestMessage {Method = method, RequestUri = requestUri};
+            var response = await _client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+            _rateLimitingTask = Task.Delay(RateLimitDelayMilliseconds);
 
+            if (response.IsSuccessStatusCode)
                 return await ReadContentAsJson<T>(response);
-            }
+
+            var scryfallError = await ReadContentAsJson<Error>(response);
+            throw scryfallError;
         }
 
         private async Task<T> ReadContentAsJson<T>(HttpResponseMessage response)
         {
-            using (var stream = await response.Content.ReadAsStreamAsync())
-            using (var streamReader = new StreamReader(stream))
-            using (var reader = new JsonTextReader(streamReader))
+            var stream = await response.Content.ReadAsStreamAsync();
+            using (var reader = new JsonTextReader(new StreamReader(stream)))
                 return _serializer.Deserialize<T>(reader);
         }
     }
