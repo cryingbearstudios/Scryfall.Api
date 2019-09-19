7d47364710268ea4af1d5f8436820f3d4b06932f
Matt Campbell <xenolinguist@gmail.com>
2019-09-18 20:28:42 -0500

    Add convenience properties to BulkDataList

---
+++ src/Scryfall.Api/BulkDataList.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Scryfall.Api
+{
+    public partial class BulkDataList
+    {
+        public BulkData this[BulkDataType key] => Data.Find(d => d.Type == key);
+
+        public BulkData Rulings => this[BulkDataType.Rulings];
+        public BulkData DefaultCards => this[BulkDataType.DefaultCards];
+        public BulkData AllCards => this[BulkDataType.AllCards];
+        public BulkData OracleCards => this[BulkDataType.OracleCards];
+    }
+}
